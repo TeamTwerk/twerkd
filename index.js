0b4e1470a066263b1e0f35626414c9871518850f
Dan Tame <dantame@Dans-MacBook-Pro.local>
2014-08-02 15:23:06 +0100

    Updated server, added matchmaking, join / leave rooms, pass through of data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var ioredis = require('socket.io-redis');
+var _ = require('lodash');
+var shortId = require('shortid');
 
 server.listen(3000);
 io.adapter(ioredis({host: 'localhost', port: 6379}));
@@ -11,30 +13,100 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  console.log("CONNECT", socket.id);
-  socket.join('lobby');
-  for (var i = 0; i < io.sockets.length; i++) {
-    console.log(io.sockets[i].rooms)
-    console.log("----------");
-  }
-  /*for (s in io.sockets) {
-    console.log(s);
-    console.log("test");
-  }*/
-  //console.log(io.sockets);
+  socket.emit('data', {m: 'Patrick Stuff', c: { tpm: 12 } });
   socket.on('join', function (data) {
-    socket.join(data.roomid);
+    joinRoom(socket, data.roomId);
   });
   socket.on('leave', function (data) {
-    socket.leave(data.roomid);
+    leaveRoom(socket, data.roomId);
   });
   socket.on('data', function (data) {
-
+    socket.broadcast.to(data.c.roomId).emit('data', {m: 'twerk', c: data.c.twerk});
   });
-  socket.on('matchmaking', findOpponent);
+  socket.on('matchmaking', function (data) {
+    switch (data.m) {
+      case "join":
+        joinMatchmaking(socket, data);
+        break;
+      case "leave":
+        leaveMatchmaking(socket, data);
+        break;
+      case "start":
+        startGame(socket, data);
+        break;
+      case "end":
+        endGame(socket, data);
+        break;
+    }
+  });
 });
 
-function findOpponent(data) {
-  socket.join('lobby');
+function startGame(socket, data) {
+  var opponent = findOpponent(socket, data);
+  var randomRoomId = shortId.generate();
+  if (opponent) {
+      joinRoom(opponent, randomRoomId);
+      joinRoom(socket, randomRoomId);
+      socket.emit('matchmaking', {m: 'joinRoom', c: {roomId: randomRoomId, opponent: opponent.id}});
+      leaveRoom(opponentPool[0], 'lobby');
+    } else {
+      socket.emit('error', {code: 404, msg: "Could not find opponent"})
+    }
+    leaveRoom(socket, 'lobby');
+}
 
+function endGame(socket, data) {
+  joinRoom(socket, 'lobby');
+}
+
+function joinMatchmaking(socket, data) {
+  joinRoom(socket, 'lobby');
+}
+
+function leaveMatchmaking(socket, data) {
+  leaveRoom(socket, 'lobby');
+}
+
+function findOpponent(socket, data) {
+  joinRoom(socket, 'lobby');
+
+  var opponentPool = getSocketsInRoom('lobby');
+
+  _.pull(opponentPool, socket);
+
+  opponentPool = _.shuffle(opponentPool);
+
+  return opponentPool.length > 0 ? opponentPool[0] : false;
+}
+
+function joinRoom(socket, roomId) {
+  socket.join(roomId);
+  var roomSockets = getSocketsInRoom(roomId);
+  var roomSocketIds = _.map(roomSockets, function (s) {
+    return s.id;
+  });
+  socket.to(roomId).emit({m: 'updateRoom', c:{ users: roomSocketIds }});
+}
+
+function leaveRoom(socket, roomId) {
+  socket.leave(roomId);
+  var roomSockets = getSocketsInRoom(roomId);
+  var roomSocketIds = _.map(roomSockets, function (s) {
+    return s.id;
+  });
+  socket.to(roomId).emit({m: 'updateRoom', c:{ users: roomSocketIds }});
+}
+
+function getSocketsInRoom(roomId) {
+  var roomSockets = [];
+
+  io.sockets.sockets.forEach(function (s) {
+    s.rooms.forEach(function (id) {
+        if (id === roomId) {
+          roomSockets.push(s);
+        }
+    });
+  });
+
+  return roomSockets;
 }
