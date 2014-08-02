99c934ddc1c7c114d417dbd15e8a15b86381f221
Patrick Mead <patrick.mead@dubitlimited.com>
2014-08-03 00:46:42 +0100

    Chore, minor refactor of session controller

--- client/app/scripts/controllers/session.js
+++ client/app/scripts/controllers/session.js
@@ -8,77 +8,83 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('SessionCtrl', function (mySocket,$scope,$routeParams) {  	
-  $scope.page = 0;
+  .controller('SessionCtrl', function (mySocket,$scope,$routeParams) {  
 
+  var users = [];
+
+  $scope.page = 0;
   $scope.numPages = 3; //todo update this with the number of pages in the switch statement in session.html
 
-  initDebugData();
+  var twerkHistory = [{"x": "1", "y": [0,0]}];
+
+  initGraph(twerkHistory);
 
   //Join the room that was passed in the url (route params)
-	mySocket.emit('join', {c:{ roomId: $routeParams.roomid} });
-  	
-  	mySocket.on('data', function(data) {
-  		switch(data.m) {
-  			case 'twerk':
-  				handleTwerk(data.c);
-  				break;
-  		}
-  	});
+	joinRoom(mySocket,$routeParams.roomid);
+  listenToSocket(mySocket);
 
-    //Simple pagination function
-    $scope.pageLeft = function pageLeft() {
-      $scope.page = Math.max($scope.page - 1, 0);
-    }
+  /* Scope functions */
 
-    $scope.pageRight = function pageRight() {
-      $scope.page = Math.min($scope.page + 1, $scope.numPages-1);
-    }
+  //Simple pagination function
+  $scope.pageLeft = function pageLeft() {
+    $scope.page = Math.max($scope.page - 1, 0);
+  }
 
-  	function handleTwerk(data) {
-  		$scope.tpm = data['tpm'];
-  		$scope.t = Math.round(data['t']);
-    }
+  $scope.pageRight = function pageRight() {
+    $scope.page = Math.min($scope.page + 1, $scope.numPages-1);
+  }
 
-    function initDebugData() {
-       $scope.graphData = {
-        "series": [
-          "Player 45",
-          "Player 51"
-        ],
-        "data": [
-          {
-            "x": "15",
-            "y": [
-              5,12
-            ]
-          },
-          {
-            "x": "30",
-            "y": [
-              30,15
-            ]
-          },
-          {
-            "x": "45",
-            "y": [
-              61,56
-            ]
-          }
-        ]
+  /* Controller functions */
+
+  function joinRoom(io,roomid) {
+    io.emit('join', {c:{ roomId: roomid, spectator: true} });
+  }
+
+  function listenToSocket(io) {
+    io.on('data', function(data) {
+      console.log(data);
+      switch(data.m) {
+        case 'updateRoom':
+          handleRoomUpdate(data.c);
+          break;
+        case 'twerk':
+          handleTwerk(data.c);
+          break;
       }
+    });
+  }
 
-      $scope.graphConfig = {
-        title: 'Twerk-O-Meter',
-        tooltips: true,
-        labels: false,
-        legend: {
-          display: true,
-          position: 'left'
-        },
-        lineLegend: 'lineEnd' // can be also 'traditional'
-      }
+  function handleRoomUpdate(data) {
+    users = data['users'];
+  }
 
-      $scope.chartType = 'line';
+	function handleTwerk(data) {
+		$scope.tpm = data['tpm'];
+		$scope.t = Math.round(data['t']);
+
+    twerkHistory.push({'x': twerkHistory.length, 'y': [data['t'],data['t']]});
+  }
+
+  function initGraph(data) {
+     $scope.graphData = {
+      "series": [
+        "Player 45",
+        "Player 51"
+      ],
+      "data": data
     }
+
+    $scope.graphConfig = {
+      title: 'Twerk-O-Meter',
+      tooltips: true,
+      labels: false,
+      legend: {
+        display: true,
+        position: 'left'
+      },
+      lineLegend: 'lineEnd' // can be also 'traditional'
+    }
+
+    $scope.chartType = 'line';
+  }
 });
