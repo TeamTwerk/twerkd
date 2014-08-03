e8ab7c44a40edd3ed2d14c4da2a1b99b12c8a77e
Patrick Mead <patrick.mead@dubitlimited.com>
2014-08-03 02:44:25 +0100

    Updated battle view

--- client/app/scripts/controllers/session.js
+++ client/app/scripts/controllers/session.js
@@ -10,12 +10,14 @@
 angular.module('clientApp')
   .controller('SessionCtrl', function (mySocket,$scope,$routeParams) {  
 
-  var users = [];
+  var players = [];
 
   $scope.page = 0;
   $scope.numPages = 3; //todo update this with the number of pages in the switch statement in session.html
 
+  var userData = {};
   var twerkHistory = [{"x": "1", "y": [0,0]}];
+
 
   initGraph(twerkHistory);
 
@@ -54,13 +56,41 @@ angular.module('clientApp')
     });
   }
 
+  $scope.playerData = function(index) {
+    if(players.length-1 < index) return  { 't' : 0, 'tpm' : 0};
+
+    var dat = userData[players[index].id];
+
+    if(dat == null || dat.length == 0) return  { 't' : 0, 'tpm' : 0};
+
+    var last = dat[dat.length-1];
+
+    return last;
+  }
+
+  $scope.barProgress = function() {
+    var player1Twerks = 500;
+    var player2Twerks = 25;
+
+    return (player1Twerks / (player1Twerks + player2Twerks)) * 640; //magic number bar width!
+  }
+
   function handleRoomUpdate(data) {
-    users = data['users'];
+    players = data['users'].filter(filterSpectators);
+  }
+
+  function filterSpectators(value,index,ar) {
+    return value['spectator'] != true;
   }
 
 	function handleTwerk(data) {
-		$scope.tpm = data['tpm'];
-		$scope.t = Math.round(data['t']);
+    var twerk = data['twerk'];
+		$scope.tpm = twerk['tpm'];
+		$scope.t = Math.round(twerk['t']);
+    var id = data['id'];
+
+    if(userData[id] == null) userData[id] = [];
+    userData[id].push(twerk);
 
     twerkHistory.push({'x': twerkHistory.length, 'y': [data['t'],data['t']]});
   }
